a247d5d06496d70652381f71caed45b180a0e2f0
LanceHuynh <34030826+LanceHuynh@users.noreply.github.com>
2024-01-14 19:05:58 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,4 +38,6 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
-console.log('Server is running on port: ' + PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
